ccf2253e33c86690f4722857e81862117eb41296
Soya <Axewu_Reitzel@hotmail.com>
2025-07-22 00:03:44 +0200

    Player Movement Animations
    Player movement animations for all directions working as intended

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,32 @@
 extends CharacterBody2D
 
+@onready var animation_player = $PlayerAnimation
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * 200
 	move_and_slide()
+	handle_animations(direction)
+
+func handle_animations(direction: Vector2) -> void:
+	if direction.length() == 0:
+		animation_player.play("idle")
+		return
+
+	if direction.x < 0 && direction.y == 0:        # Left
+		animation_player.flip_h = false
+		animation_player.play("walk_left-right")
+	elif direction.x > 0 && direction.y == 0:      # Right
+		animation_player.flip_h = true
+		animation_player.play("walk_left-right")
+	elif direction.x < 0 && direction.y != 0:      # Diagonal Left (both up and down)
+		animation_player.flip_h = false
+		animation_player.play("walk_diagonal")
+	elif direction.x > 0 && direction.y != 0:      # Diagonal Right (both up and down)
+		animation_player.flip_h = true
+		animation_player.play("walk_diagonal")
+	else:                                          # Up and Down
+		animation_player.play("walk_up-down")
+		
+
+		
